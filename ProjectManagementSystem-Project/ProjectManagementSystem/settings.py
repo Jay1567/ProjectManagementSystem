e54d07fb8445dbf27b6350899797d02b0ed2fd1a
Jay Kothari <kotharijay65@gmail.com>
2021-12-20 09:58:05 +0530

    Adding pagination

--- ProjectManagementSystem-Project/ProjectManagementSystem/settings.py
+++ ProjectManagementSystem-Project/ProjectManagementSystem/settings.py
@@ -80,6 +80,8 @@ REST_FRAMEWORK = {
         'rest_framework.authentication.SessionAuthentication',
         'rest_framework.authentication.TokenAuthentication'
     ],
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 10
 }
 
 SITE_ID = 1
